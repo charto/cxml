6b6c76ac43d23c65dbda813ffa86ad805172634b
jjrv <juha@busfaster.com>
2016-03-03 12:57:16 +0200

    Add streaming hooks to example.

--- test/dir-example.ts
+++ test/dir-example.ts
@@ -5,9 +5,26 @@ import * as example from 'cxml/test/xmlns/dir-example';
 
 var parser = new cxml.Parser();
 
+parser.attach(class DirHandler extends (example.document.dir.constructor) {
+
+	/** Fires when the opening <dir> and attributes have been parsed. */
+
+	_before() {
+		console.log('\nBefore ' + this.name + ': ' + JSON.stringify(this));
+	}
+
+	/** Fires when the closing </dir> and children have been parsed. */
+
+	_after() {
+		console.log('After  ' + this.name + ': ' + JSON.stringify(this));
+	}
+
+});
+
 var result = parser.parse('<dir name="empty"></dir>', example.document);
 
 result.then((doc: example.document) => {
+
 	console.log('\n=== empty ===\n');
 
 	console.log( JSON.stringify(doc) );  // {"dir":{"name":"empty"}}
@@ -21,12 +38,15 @@ result.then((doc: example.document) => {
 
 	console.log( dir._exists );          // true
 	console.log( dir.file[0]._exists );  // false (not an error!)
+
 });
 
 result = parser.parse(fs.createReadStream(path.resolve(__dirname, 'xml/dir-example.xml')), example.document);
 
 result.then((doc: example.document) => {
+
 	console.log('\n=== 123 ===\n');
 
 	console.log(JSON.stringify(doc, null, 2));
+
 });
