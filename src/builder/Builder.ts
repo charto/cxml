d3260953d5936a06011c5d86125302e99034ec91
jjrv <juha@busfaster.com>
2017-06-01 14:53:19 +0300

    Support lists as attributes and allow setting expected root elements.

--- src/builder/Builder.ts
+++ src/builder/Builder.ts
@@ -10,6 +10,7 @@ const enum State {
 	COMMENT
 }
 
+/*
 export class ElementSpec {
 	tagName: string;
 	exists?: boolean;
@@ -22,66 +23,87 @@ export class Element {
 	xmlns: Namespace;
 	_: ElementSpec;
 }
+*/
 
-interface RuleRef {
-	rule: Rule;
+interface Rule {
+	type: RuleType;
 	isArray?: boolean;
 }
 
-class Rule {
-	elements: { [ elementName: string ]: RuleRef } = {};
-	attributes: { [ attributeName: string ]: boolean } = {};
+class RuleType {
+	elements: { [ elementName: string ]: Rule } = {};
+	attributes: { [ attributeName: string ]: Rule } = {};
 
-	static string = new Rule();
+	static string = new RuleType();
 }
 
 export class Builder {
-	constructor(schema: any) {
-		const ruleTbl: { [ typeName: string ]: Rule } = {
-			string: Rule.string
+	constructor(schema: any, roots = schema.document) {
+		const typeTbl: { [ typeName: string ]: RuleType } = {
+			roots: new RuleType(),
+			string: RuleType.string
 		};
 		let parts: RegExpMatchArray | null;
 
 		let prefix: string, name: string, suffix: string;
-		let rule: Rule;
+		let type: RuleType;
 
 		for(let typeName of Object.keys(schema)) {
-			ruleTbl[typeName] = new Rule();
+			typeTbl[typeName] = new RuleType();
 		}
 
-		for(let typeName of Object.keys(schema)) {
-			rule = ruleTbl[typeName];
+		let memberTbl: { [ name: string ]: Rule };
+		let memberName: string;
+		let memberType: RuleType;
 
-			for(let child of schema[typeName]) {
+		for(let typeName of Object.keys(typeTbl)) {
+			type = typeTbl[typeName];
+			let spec = schema[typeName];
+
+			if(!spec) {
+				if(typeName == 'roots') spec = roots;
+				else continue;
+			}
+
+			for(let child of spec) {
 				if(typeof(child) == 'string') {
-					parts = child.match(/([$]?)([^\[]+)(\[\])?/);
-					if(parts) {
-						[, prefix, name, suffix] = parts;
+					memberName = child;
+					child = {};
+					child[memberName] = memberName;
+				}
 
-						if(prefix == '$') {
-							rule.attributes[name] = true;
-						} else if(suffix == '[]') {
-							rule.elements[name] = { rule: ruleTbl[name], isArray: true };
-						} else {
-							rule.elements[name] = { rule: ruleTbl[name] };
-						}
+				for(memberName of Object.keys(child)) {
+					parts = child[memberName].match(/([$]?)([^\[]+)(\[\])?/);
+					if(!parts) continue;
+
+					[, prefix, name, suffix] = parts;
+					if(memberName == child[memberName]) memberName = name;
+
+					if(prefix == '$') {
+						memberTbl = type.attributes;
+						memberType = RuleType.string;
+					} else {
+						memberTbl = type.elements;
+						memberType = typeTbl[name];
 					}
-				} else {
-					for(let key of Object.keys(child)) {
-						rule.elements[key] = { rule: ruleTbl[child[key]] };
-					}
+
+					memberTbl[memberName] = {
+						type: memberType,
+						isArray: suffix == '[]'
+					};
 				}
 			}
 		}
 
-		this.documentRule = ruleTbl['document'];
+		this.rootRule = { type: typeTbl['roots'] };
 	}
 
 	build(parser: Parser, cb: any) {
 		const document: any = {};
 		let stackPos = 0;
 
-		let rule = this.documentRule;
+		let rule = this.rootRule;
+		let ruleNext: Rule | undefined;
 		const ruleStack: Rule[] = [];
 
 		let item = document;
@@ -94,8 +116,6 @@ export class Builder {
 		parser.on('data', (chunk: TokenChunk) => {
 			let buffer = chunk.buffer;
 			let token: Token | number | string;
-			let ruleRef: RuleRef;
-			let ruleNext: Rule | undefined;
 			let name: string;
 			const lastNum = chunk.last;
 			let tokenNum = -1;
@@ -115,8 +135,7 @@ export class Builder {
 						case TokenKind.open:
 
 							name = (token as OpenToken).name;
-							ruleRef = rule.elements[name];
-							ruleNext = ruleRef.rule;
+							ruleNext = rule.type.elements[name];
 
 							if(!ruleNext) {
 								++depth;
@@ -125,12 +144,14 @@ export class Builder {
 								break;
 							}
 
-							if(ruleNext == Rule.string) {
+							if(ruleNext.type == RuleType.string) {
+								// NOTE: If the string element has attributes,
+								// they're added to its parent element!
 								target = name;
 								itemNext = item;
 							} else {
 								itemNext = {};
-								if(ruleRef.isArray) {
+								if(ruleNext.isArray) {
 									if(!item[name]) item[name] = [];
 									item[name].push(itemNext);
 								} else item[name] = itemNext;
@@ -147,6 +168,8 @@ export class Builder {
 
 						case TokenKind.emitted:
 
+							if(rule.type == RuleType.string) ruleNext = rule;
+
 							state = State.TEXT;
 							break;
 
@@ -161,7 +184,8 @@ export class Builder {
 						case TokenKind.string:
 
 							name = (token as StringToken).name;
-							if(rule.attributes[name]) target = name;
+							ruleNext = rule.type.attributes[name];
+							if(ruleNext) target = name;
 
 							break;
 
@@ -179,8 +203,8 @@ export class Builder {
 						// Fallthru
 						case State.ELEMENT:
 
-							if(target) {
-								item[target] = token;
+							if(ruleNext && target) {
+								item[target] = ruleNext.isArray ? (token + '').split(/ +/) : token;
 								target = null;
 							}
 
@@ -193,5 +217,5 @@ export class Builder {
 		parser.on('end', () => cb(document));
 	}
 
-	private documentRule: Rule;
+	private rootRule: Rule;
 }
